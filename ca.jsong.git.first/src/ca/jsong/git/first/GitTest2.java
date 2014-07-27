31be56ad2a47f9489d3238078fb275d8bf66c4c1
jiajian24 <jiajian24@yahoo.com>
2014-07-26 20:23:00 -0400

    branch3

--- ca.jsong.git.first/src/ca/jsong/git/first/GitTest2.java
+++ ca.jsong.git.first/src/ca/jsong/git/first/GitTest2.java
@@ -5,7 +5,8 @@ public class GitTest2 {
 	public static void main(String[] args) {
 		System.out.println("Hello World 1");
 		System.out.println("Hello World 2");
-
+		System.out.println("Hello World 2 branch3");
+ 
 	}
 
 }
