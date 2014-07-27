c6ed657af0850fe52328b4a28cde0a1f452b1957
jiajian24 <jiajian24@yahoo.com>
2014-07-26 20:34:43 -0400

    master change2

--- ca.jsong.git.first/src/ca/jsong/git/first/GitTest2.java
+++ ca.jsong.git.first/src/ca/jsong/git/first/GitTest2.java
@@ -5,6 +5,7 @@ public class GitTest2 {
 	public static void main(String[] args) {
 		
 		System.out.println("master change 1 1 ");
+		System.out.println("master change 1 2 ");
 		System.out.println("Hello World 1");
 		System.out.println("Hello World 2");
 
